fbe26a1416ddc1cae1fe0917945021e9ff075299
Abbas Sarwar <abbassarwar100@gmail.com>
2023-05-01 17:15:14 +0500

    Header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import TodoApp from './TodoApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoApp />
   </React.StrictMode>
 );
 
